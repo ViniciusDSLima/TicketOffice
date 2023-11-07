0d5525e3cb293980f225c1308afa7005b3efc586
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-11-07 13:58:52 -0300

    Criada a classe principal de configuracao do Spring Security e adicionado os metodos de gerencia de autenticacao, filtros de autenticacao de requisicoes e tambem encode de senhas.

---
+++ src/main/java/com/example/ticketoffice/security/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.ticketoffice.security.config;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    AuthFilter authFilter;
+
+    final String[] PERMISSOES = {
+            "/user/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "api-docs/**",
+            "v3/**"
+    };
+
+    @SneakyThrows(Exception.class)
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security){
+        return security.authorizeHttpRequests((auth -> auth.requestMatchers(PERMISSOES)
+                .permitAll().anyRequest().authenticated())).addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
+    @SneakyThrows(Exception.class)
+    @Bean
+    public AuthenticationManager getAuthenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
