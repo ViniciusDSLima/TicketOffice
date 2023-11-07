909104769cb310dc08ebe76f89ce935fd70481e1
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-11-07 13:02:34 -0300

    Criado o TokenService e seus metodos de criacao e validacao do token jwt

---
+++ src/main/java/com/example/ticketoffice/security/service/TokenService.java
@@ -0,0 +1,36 @@
+package com.example.ticketoffice.security.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Calendar;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    SecretKey Key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String email){
+        return Jwts.builder().claim("email", email).signWith(Key).setExpiration(getDate()).compact();
+    }
+
+    private Date getDate(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR,3);
+
+        return calendar.getTime();
+    }
+
+    private boolean validateToken(String token){
+        return getClaims(token).getExpiration().after(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(Key).build().parseClaimsJws(token).getBody();
+    }
+}
