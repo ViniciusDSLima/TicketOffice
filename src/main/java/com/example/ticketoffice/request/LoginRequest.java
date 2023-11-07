53ec6cf28b4d22b2b00731c0441818c276a71bd7
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-11-07 14:50:46 -0300

    Criada a classe de requisicao de login do usuario.

---
+++ src/main/java/com/example/ticketoffice/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.example.ticketoffice.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}
