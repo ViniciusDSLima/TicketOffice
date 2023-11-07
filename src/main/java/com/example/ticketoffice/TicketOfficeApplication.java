5830d8f8e3652ac4ae10139062404de393700169
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-11-07 12:11:41 -0300

    Started the project with the following dependencias and libraries: Lombok, spring security, spring data jpa, spring web, validation, mapstruct, postgreSQL

---
+++ src/main/java/com/example/ticketoffice/TicketOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketOfficeApplication.class, args);
+    }
+
+}
